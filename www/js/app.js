fa9abaacca27d836ed68e108316620c05aa9b9d3
Joel Nation <joel.nation@outlook.com>
2016-07-30 22:02:16 +1000

    Added About page

--- www/js/app.js
+++ www/js/app.js
@@ -61,6 +61,14 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     }
   })
+  .state('app.about', {
+    url: '/about',
+    views : {
+      'menuContent': {
+        templateUrl: 'templates/about.html'
+      }
+    }
+  })
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/map');
 });
